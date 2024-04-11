05bb2976a34aef6731159f231f5a7beb1afe7b8d
starik061 <starik061@gmail.com>
2024-04-11 18:09:29 +0300

    first inicialization and creating base structure of FilterPanel component

---
+++ src/components/FilterPanel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="filter-panel">
+        test
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.filter-panel {
+    background-color: red;
+}
+</style>
